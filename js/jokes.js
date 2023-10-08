f837aa3eb8d88c22affd4d34ca0bf244707da039
Elizabeth Kodjo <ellizakodjo@outlook.com>
2023-10-08 19:18:52 +0100

    Completed 1st project, started another

--- js/jokes.js
+++ js/jokes.js
@@ -1,7 +1,38 @@
 const btnEl = document.getElementById('btn');
+const jokeEL = document.getElementById('joke');
+const apiKey = '+wIy9FeXgA0KM0XtZ1sxEQ==e7ju3Q3Q65ZJ9I3j';
 
-function getJoke() {
-    console.log('I clicked');
+const options = {
+    method: 'GET',
+    headers: {
+        'X-Api-Key': apiKey,
+    },
+};
+
+const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1';
+
+async function getJoke() {
+
+    try {
+        jokeEL.innerText = 'Updating...';
+        btnEl.disabled = true;
+        btnEl.innerText = 'Loading...';
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
+
+        btnEl.disabled = false;
+        btnEl.innerText = 'Tell me a joke';
+
+        jokeEL.innerText = data[0].joke;
+
+    } catch (error) {
+        jokeEL.innerText = 'An error happened, try later.';
+        btnEl.disabled = false;
+        btnEl.innerText = 'Tell me a joke';
+        console.log(error);
+
+    }
+
 }
 
-btnEl.addEventListener('click', getJoke)
+btnEl.addEventListener('click', getJoke);
